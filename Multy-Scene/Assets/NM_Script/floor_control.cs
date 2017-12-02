38c130017ad6dcfcfcff157f68f204e2399430e5
bihusenlan <zhouniyao@163.com>
2017-12-02 12:37:12 +0800

    fesh

---
+++ Multy-Scene/Assets/NM_Script/floor_control.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class floor_control : MonoBehaviour {
+	public static  float WIDTH = 500.0f;
+	public static float HEIGHT = 500.0f;
+
+	public GameObject[] terrain;
+	public GameObject original_terrain;
+	// Use this for initialization
+	void Start () {
+		//root = GameObject.Find("Show");
+		Instantiate(original_terrain, new Vector3(0, 0, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 camera_position = this.transform.position;//摄像机位置
+		//Debug.Log("x:" + camera_position.x + ", y:" + camera_position.y + ", z:" + camera_position.z);
+		//string curName = SceneManager.GetActiveScene().name;//获取当前场景的名称
+
+		//int flag = (int)camera_position.x / WIDTH;
+		if (camera_position.x > 250) {
+			Instantiate(terrain[0], new Vector3(500, 0 , 0), Quaternion.identity);
+		
+		}
+
+
+		/*
+		if (camera_position.x > 250.0f) {
+			Vector3 next_terrain_position = new Vector3(camera_position.x + 250, 0, 0);
+			//找到物体
+			//GameObject next_terrain = root.transform.Find("NewTerrain").gameObject;
+			GameObject next_terrain = terrain[0];
+			next_terrain.SetActive (true);
+			Instantiate(next_terrain, next_terrain_position, Quaternion.identity);
+
+			//SceneManager.LoadScene(name);
+			//SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+		if (camera_position.z > 250.0f) {
+			Vector3 next_terrain_position = new Vector3(0, 0, camera_position.x + 250);
+			//找到物体
+			//GameObject next_terrain = root.transform.Find("NewTerrain").gameObject;
+			GameObject next_terrain = terrain[0];
+			next_terrain.SetActive (true);
+			Instantiate(next_terrain, next_terrain_position, Quaternion.identity);
+
+		}
+		
+		*/
+
+	}
+
+
+
+
+}
---
+++ Multy-Scene/Assets/NM_Script/terrain_nm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrain_nm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	// 其他游戏对象跑出屏幕之外时的处理
+	void OnBecameInvisible()
+	{
+		// 销毁该游戏对象
+		// 注意这里的参数！
+		// Destory(this) 只会销毁组件
+		Destroy(this.gameObject);
+	}
+}
